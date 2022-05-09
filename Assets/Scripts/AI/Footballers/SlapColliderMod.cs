6cbfaa7ef4c245c5c69484261fb9b164c37d7adf
Abhinav <as3368@live.mdx.ac.uk>
2022-05-09 17:18:01 +0100

    Slap trigger enable/disable

---
+++ Assets/Scripts/AI/Footballers/SlapColliderMod.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlapColliderMod : MonoBehaviour
+{
+    public Collider SlapCollider;
+
+    public void SlapColliderEnable()
+    {
+        SlapCollider.enabled = true;
+    }
+    public void SlapColliderDisable()
+    {
+        SlapCollider.enabled = false;
+    }
+}
